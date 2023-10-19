b36adabc691cb13c16d7e92fdd87b082dcdc8f9d
Muhamad Abdullah <abdulx125@gmail.com>
2023-10-19 01:44:52 -0700

    ..

---
+++ app.js
@@ -0,0 +1,6 @@
+let startNode = document.getElementById('Start')
+let stopNode = document.getElementById('Stop')
+let pauseNode = document.getElementById('Pause')
+let resetNode = document.getElementById('Reset')
+
+// let's make a stop watch
